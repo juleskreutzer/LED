03945f71b8a720783a26cba66217de01add13f8f
Jules Kreutzer <juleskreutzer@me.com>
2016-03-29 13:36:52 +0200

    Initial commit for iOS app

---
+++ LED-iOS/LED-iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LED-iOS
+//
+//  Created by Jules Kreutzer on 29-03-16.
+//  Copyright © 2016 Jules Kreutzer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
